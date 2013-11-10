05a0c2ad2a979f7e31eda34d032fecfc64590171
fabiansch <stahlschiff@web.de>
2013-11-10 12:34:04 +0100

    bug fix price validation for creating a new cookbook

--- app/models/cookbook.rb
+++ app/models/cookbook.rb
@@ -2,4 +2,5 @@ class Cookbook < ActiveRecord::Base
 	validates :isbn, presence: true
 	validates :author, presence: true
 	validates :title, presence: true
+	validates :price, numericality: true
 end
